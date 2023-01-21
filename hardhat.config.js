cf4a6d5ea4fae5e0acdbf0891642b96116e6b792
e.mercado <admin@wayalabs.com>
2023-01-20 19:11:13 -0500

    update files

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,18 +14,17 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
     solidity: "0.8.17",
     defaultNetwork: "hyperspace", yarn
-
-    network: {
-        hyperspace: {
-            chainId: 3141,
-            url: "https://api.hyperspace.node.glif.io/rpc/v1",
-            accounts: [process.env.PRIVATE_KEY],
-        },
-    },
+},
+    network; {
+    hyperspace: {
+        chainId: 3141,
+            url; "https://api.hyperspace.node.glif.io/rpc/v1",
+                accounts;[process.env.PRIVATE_KEY]
+    }
     paths: {
         sources: "./contracts",
-        tests: "./test",
-        cache: "./cache",
-        artifacts: "./artifacts",
-    },
+            tests; "./test",
+                cache; "./cache",
+                    artifacts; "./artifacts",
+    }
 }
