2e0c42586ce1fe9e3529a99e56e941e9ab75f896
e.mercado <admin@wayalabs.com>
2023-01-23 01:08:30 -0500

    update spaceCoin.sol

--- contracts/hackathon/SpaceCoin.sol
+++ contracts/hackathon/SpaceCoin.sol
@@ -5,22 +5,26 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol"
 import "contracts/lib/openzeppelin-contracts/contracts/utils/math/SafeMath.sol"
 
-contract SpaceCoin is ERC20 {
+contract SpaceCoin is ERC20Interface { 
+    address public owner;
     string public constant name = "Space Coin";
     string public constant symbol = "SPACE";
     uint8 public constant decimals = 18;
-    uint256 public totalSupply = 10000;
+    uint256 public totalSupply = 100000;
 
     mapping (address => uint) public balanceOf;
+    mapping (address => uint256) public balances;
     mapping (address => mapping (address => uint256)) public allowance;
  
-    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Minted(address indexed _from, address indexed _to, uint256 _value);
 
     constructor() public {
+        owner = msg.sender;
         name = "LottoCoin";
-        symbol = "LOT";
+        symbol = "SPACE";
         decimals = 18;
-        totalSupply = 100000000 * (10 ** uint256(decimals));
+        totalSupply = 100000 * (10 ** uint256(decimals));
         balanceOf[msg.sender] = totalSupply;
 
         constructor(uint256 _supply) public {
@@ -33,17 +37,26 @@ contract SpaceCoin is ERC20 {
         _;
     }
 
-  
-    function transfer(address to, uint256 amount) public returns (bool success) {
-    require(balanceOf[msg.sender] >= amount);
-    balanceOf[msg.sender] -= amount;
-    balanceOf[to] += amount;
-    Transfer(msg.sender, to, amount);
-    return true;
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
     }
-        function setTotalSupply(uint256 _supply) public {
-        totalSupply = _supply;
-    } 
+
+    function mint(address _to, uint256 _value) public onlyOwner {
+        require(_value > 0, "Mint amount must be greater than 0");
+        balances[_to] += _value;
+        totalSupply += _value;
+        emit Transfer(address(0), _to, _value);
+        emit Minted(msg.sender, _to, _value);
+    }
+  
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(balances[msg.sender] >= _value && _value > 0 && allowed[msg.sender], "Transfer failed");
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[_from] >= _value);
@@ -65,11 +78,12 @@ contract SpaceCoin is ERC20 {
         return totalSupply;
     }
 
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balanceOf[_owner];
-    }
-
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return allowance[_owner][_spender];
+    }
+
+    modifier transfer(address _to, uint256 _value) {
+        require(balanceOf[msg.sender] >= _value);
+        _;
     }
 }
