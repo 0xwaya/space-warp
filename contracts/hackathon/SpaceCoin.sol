31c560c2b33a113c6507e0fdc96862e7fc579f1c
e.mercado <admin@wayalabs.com>
2023-01-27 12:43:25 -0500

    update optimized spacecoin.sol

--- contracts/hackathon/SpaceCoin.sol
+++ contracts/hackathon/SpaceCoin.sol
@@ -2,64 +2,61 @@
 
 /**
  * @title SpaceCoin
- * @dev SpaceCoin is a token used in the Space Warp hackathon
+ * @dev @0xwaya created SpaceCoin for the Space Warp hackathon
  */
+
 
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract SpaceCoin is ERC20{ 
+contract SpaceCoin is ERC20 {
+    using SafeMath for uint256;
+
     address public owner;
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
- 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Minted(address indexed _from, address indexed _to, uint256 _value);
 
-        constructor() public {
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    constructor() public {
         owner = msg.sender;
-        name = "SpaceCoin";
-        symbol = "SPACE";
         decimals = 18;
         totalSupply = 100000 * (10 ** uint256(decimals));
     }
-        modifier transfer(address _to, uint256 _value) {
-        require(balanceOf[msg.sender] >= _value);
-        _;
-    }
+
     function mint(address _to, uint256 _value) public {
         require(_value > 0, "Mint amount must be greater than 0");
-        balances[_to] += _value;
-        totalSupply += _value;
+        require(_to != address(0), "Invalid address");
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        totalSupply = totalSupply.add(_value);
         emit Transfer(address(0), _to, _value);
-        emit Minted(msg.sender, _to, _value);
     }
-  
-      /** * @dev Function to transfer tokens from one address to another
-          * @param _to The address to transfer tokens to
-          * @param _value The amount of tokens to transfer */
 
     function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0), "Invalid address");
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        _to.transfer(_value);
         return true;
     }
 
-
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);
-        require(allowance[_from][msg.sender] >= _value);
+        require(_from != address(0), "Invalid address");
+        require(_to != address(0), "Invalid address");
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        require(allowance[_from][msg.sender] >= _value, "Insufficient allowance");
         balanceOf[_from] = balanceOf[_from].sub(_value);
-        balanceOf[_to] = balanceOf[_to].add(_value);
+        _to.transfer(_value);
         allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
         emit Transfer(_from, _to, _value);
         return true;
     }
 
     function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0), "Invalid address");
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
@@ -72,5 +69,4 @@ contract SpaceCoin is ERC20{
     function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
         return allowance[_owner][_spender];
     }
-
 }
