546277abc1cef3193c55ce4f34b13edd6c898dd1
e.mercado <admin@wayalabs.com>
2023-02-04 13:10:33 -0500

    commit to main

--- dapp/packages/react-app/src/App.js
+++ dapp/packages/react-app/src/App.js
@@ -1,24 +1,24 @@
 import React from "react"
 // eslint-disable-next-line
 import { useEthers } from '@usedapp/core';
+import styles from './styles';
 import { spaceswapLogo } from './assets';
-import { styles } from './styles';
 
 const App = () => {
   return
   {
     <div className={styles.container}>
-      <div className={styles.innerContainer}>
-        <header className={styles.header}>
-          <image
-            src={spaceswapLogo}
-            alt="spaceswap logo"
-            className="w-16 h-16
+    <div className={styles.innerContainer}>
+      <header className={styles.header}>
+        <image
+          src={spaceswapLogo}
+          alt="spaceswap logo"
+          className="w-16 h-16
             object-contain"
-          />
-        </header>
-      </div>
-    </div >
+        />
+      </header>
+    </div>
+  </div >
   }
 }
 
