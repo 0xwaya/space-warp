d30327dbef0bf9a4420a4a7490c468afc0ec4876
e.mercado <admin@wayalabs.com>
2023-02-04 13:04:24 -0500

    update

--- dapp/packages/react-app/src/App.js
+++ dapp/packages/react-app/src/App.js
@@ -5,18 +5,19 @@ import { spaceswapLogo } from './assets';
 import { styles } from './styles';
 
 const App = () => {
-  return {
-    < div className = { styles.container } >
-    <div className={styles.innerContainer}>
-      <header className={styles.header}>
-        <image
-          src={spaceswapLogo}
-          alt="spaceswap logo"
-          className="w-16 h-16
+  return
+  {
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <image
+            src={spaceswapLogo}
+            alt="spaceswap logo"
+            className="w-16 h-16
             object-contain"
-        />
-      </header>
-    </div>
+          />
+        </header>
+      </div>
     </div >
   }
 }
