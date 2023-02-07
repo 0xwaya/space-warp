6e927912d1ccd4d5928900626ed319a827483b81
e.mercado <admin@wayalabs.com>
2023-02-07 09:25:12 -0500

    commit to main

--- univ2kit/space-swapv1/v2-periphery-master/contracts/SpaceswapV1Rounter.sol
+++ univ2kit/space-swapv1/v2-periphery-master/contracts/SpaceswapV1Rounter.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
-
+import './interfaces/IUniswapV2Factory.sol';
 import './interfaces/IUniswapV2Router02.sol';
 import './libraries/UniswapV2Library.sol';
+import './libraries/TransferHelper.sol';
 import './libraries/SafeMath.sol';
 import './interfaces/IERC20.sol';
 import './interfaces/IWETH.sol';
